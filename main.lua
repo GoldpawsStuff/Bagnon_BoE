180023aaf2cd11a9bb4e546fa77cf95cf575c03d
Lars Norberg <lars@larsnorbergofficial.com>
2019-02-27 13:29:44 +0100

    1.0.9-Release
    - Update TOC metadata. 
    - Update README description and links.
    - Major code overhaul and optimization.
    - Added auto-disable if Bagnon ItemInfo is loaded.

--- main.lua
+++ main.lua
@@ -1,8 +1,14 @@
+if IsAddOnLoaded("Bagnon_ItemInfo") then 
+	return DisableAddOn((...), true) 
+end 
 
--- Using the Bagnon way to retrieve names, namespaces and stuff
 local MODULE =  ...
 local ADDON, Addon = MODULE:match("[^_]+"), _G[MODULE:match("[^_]+")]
-local BoE = Bagnon:NewModule("BindOnEquip", Addon)
+local Module = Bagnon:NewModule("BindOnEquip", Addon)
+
+-- Tooltip used for scanning
+local ScannerTip = _G.BagnonItemInfoScannerTooltip or CreateFrame("GameTooltip", "BagnonItemInfoScannerTooltip", WorldFrame, "GameTooltipTemplate")
+local ScannerTipName = ScannerTip:GetName()
 
 -- Lua API
 local _G = _G
@@ -10,108 +16,139 @@ local string_find = string.find
 local string_match = string.match
 
 -- WoW API
+local GetDetailedItemLevelInfo = _G.GetDetailedItemLevelInfo 
 local GetItemQualityColor = _G.GetItemQualityColor
 local GetItemInfo = _G.GetItemInfo
 
-local cache = {} -- cache of fontstrings
+-- WoW Strings
+local S_ITEM_BOUND1 = _G.ITEM_SOULBOUND
+local S_ITEM_BOUND2 = _G.ITEM_ACCOUNTBOUND
+local S_ITEM_BOUND3 = _G.ITEM_BNETACCOUNTBOUND
 
--- Localization (currently just enUS)
-local gameLocale = GetLocale()
+-- Localization. 
+-- *Just enUS so far. 
 local L = {
 	["BoE"] = "BoE", -- Bind on Equip 
 	["BoU"] = "BoU"  -- Bind on Use
 }
 
--- Tooltip used for scanning
-local scanner = CreateFrame("GameTooltip", "BagnonArtifactItemLevelScannerTooltip", WorldFrame, "GameTooltipTemplate")
-local scannerName = scanner:GetName()
+-- FontString Caches
+local Cache_ItemBind = {}
 
-local bindStrings = {
-	_G.ITEM_SOULBOUND,
-	_G.ITEM_ACCOUNTBOUND,
-	_G.ITEM_BNETACCOUNTBOUND
-}
-
-local scanLineForBindString = function(line)
-	local msg = line:GetText()
-	if (not msg) then
-		return
-	end
-	for i,v in pairs(bindStrings) do 
-		if string_find(msg, v) then
-			return true
-		end
-	end
+-----------------------------------------------------------
+-- Utility Functions
+-----------------------------------------------------------
+-- Update our secret scanner tooltip with the current button
+local RefreshScanner = function(button)
+	local bag, slot = button:GetBag(), button:GetID()
+	if (ScannerTip.bag ~= bag) or (ScannerTip.slot ~= slot) then 
+		ScannerTip.owner = button
+		ScannerTip.bag = bag
+		ScannerTip.slot = slot
+		ScannerTip:SetOwner(button, "ANCHOR_NONE")
+		ScannerTip:SetBagItem(button:GetBag(), button:GetID())
+	end 
 end
 
-local isItemBound = function(self)
-	scanner.owner = self
-	scanner:SetOwner(self, "ANCHOR_NONE")
-	scanner:SetBagItem(self:GetBag(), self:GetID())
+local IsItemBound = function(button)
+	-- Refresh the scanner item
+	RefreshScanner(button)
 
-	local isBound
-	for i = 2,6 do -- trying line 2 to 6 for the bind texts, don't think they are further down
-		local line = _G[scannerName.."TextLeft"..i]
+	-- We're trying line 2 to 6 for the bind texts, 
+	-- I don't think they're ever further down.
+	for i = 2,6 do 
+		local line = _G[ScannerTipName.."TextLeft"..i]
 		if (not line) then
 			break
 		end
-		if scanLineForBindString(line) then
+		local msg = line:GetText()
+		if msg and (string_find(msg, S_ITEM_BOUND1) or string_find(msg, S_ITEM_BOUND2) or string_find(msg, S_ITEM_BOUND3)) then 
 			return true
 		end
 	end
 end
 
-BoE.OnEnable = function(self)
-	hooksecurefunc (Bagnon.ItemSlot, "Update", function(self) 
-
-		--local itemLink = GetContainerItemLink(self:GetBag(), self:GetID())
-		local itemLink = self:GetItem() -- GetContainerItemLink(self:GetBag(), self:GetID())
-		if itemLink then
-
-			local itemString = string_match(itemLink, "item[%-?%d:]+")
-
-			local itemName, _, itemRarity, iLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, iconFileDataID, itemSellPrice, itemClassID, itemSubClassID, bindType, expacID, itemSetID, isCraftingReagent = GetItemInfo(itemLink)
-			
-			local itemID = tonumber(string_match(itemLink, "item:(%d+)"))
-
-			-- Display the bind status of BoE(Bind on Equip) and BoU(Bind on Use) items.
-			if (itemRarity and (itemRarity > 1)) and ((bindType == 2) or (bindType == 3)) and (not isItemBound(self)) then
-				if (not cache[self]) then
-
-					-- Adding an extra layer to get it above glow and border textures
-					local holder = _G[self:GetName().."ExtraInfoFrame"] or CreateFrame("Frame", self:GetName().."ExtraInfoFrame", self)
-					holder:SetAllPoints()
-
-					-- Using standard blizzard fonts here
-					local msg = holder:CreateFontString()
-					msg:SetDrawLayer("ARTWORK")
-					msg:SetPoint("BOTTOMLEFT", 2, 2)
-					msg:SetFontObject(_G.NumberFont_Outline_Med or _G.NumberFontNormal) 
-					msg:SetFont(msg:GetFont(), 11, "OUTLINE")
-					msg:SetShadowOffset(1, -1)
-					msg:SetShadowColor(0, 0, 0, .5)
-
-					-- Move Pawn out of the way
-					if self.UpgradeIcon then
-						self.UpgradeIcon:ClearAllPoints()
-						self.UpgradeIcon:SetPoint("BOTTOMRIGHT", 2, 0)
-					end
-
-					cache[self] = msg
-				end
-
-				local r, g, b = GetItemQualityColor(itemRarity)
-				cache[self]:SetTextColor(r * 2/3, g * 2/3, b * 2/3)
-				cache[self]:SetText((bindType == 3) and L["BoU"] or L["BoE"])
-			else 
-				if cache[self] then
-					cache[self]:SetText("")
-				end
-			end
-		else
-			if cache[self] then
-				cache[self]:SetText("")
-			end
-		end
-	end)
+-- Check if it's a caged battle pet
+local GetBattlePetInfo = function(itemLink)
+	if (string_find(itemLink, "battlepet")) then
+		local data, name = string_match(itemLink, "|H(.-)|h(.-)|h")
+		local  _, _, level, rarity = string_match(data, "(%w+):(%d+):(%d+):(%d+)")
+		return true, level or 1, tonumber(rarity) or 0
+	end
 end
+
+-----------------------------------------------------------
+-- Cache & Creation
+-----------------------------------------------------------
+-- Retrieve a button's plugin container
+local GetPluginContainter = function(button)
+	local name = button:GetName() .. "ExtraInfoFrame"
+	local frame = _G[name]
+	if (not frame) then 
+		frame = CreateFrame("Frame", name, button)
+		frame:SetAllPoints()
+	end 
+	return frame
+end
+
+local Cache_GetItemBind = function(button)
+	local ItemBind = GetPluginContainter(button):CreateFontString()
+	ItemBind:SetDrawLayer("ARTWORK")
+	ItemBind:SetPoint("BOTTOMLEFT", 2, 2)
+	ItemBind:SetFontObject(_G.NumberFont_Outline_Med or _G.NumberFontNormal) 
+	ItemBind:SetFont(ItemBind:GetFont(), 11, "OUTLINE")
+	ItemBind:SetShadowOffset(1, -1)
+	ItemBind:SetShadowColor(0, 0, 0, .5)
+
+	-- Move Pawn out of the way
+	local UpgradeIcon = button.UpgradeIcon
+	if UpgradeIcon then
+		UpgradeIcon:ClearAllPoints()
+		UpgradeIcon:SetPoint("BOTTOMRIGHT", 2, 0)
+	end
+
+	-- Store the reference for the next time
+	Cache_ItemBind[button] = ItemBind
+
+	return ItemBind
+end
+
+-----------------------------------------------------------
+-- Main Update
+-----------------------------------------------------------
+local Update = function(self)
+	local itemLink = self:GetItem() 
+	if itemLink then
+
+		-- Get some blizzard info about the current item
+		local itemName, _itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, iconFileDataID, itemSellPrice, itemClassID, itemSubClassID, bindType, expacID, itemSetID, isCraftingReagent = GetItemInfo(itemLink)
+		local effectiveLevel, previewLevel, origLevel = GetDetailedItemLevelInfo(itemLink)
+		local isBattlePet, battlePetLevel, battlePetRarity = GetBattlePetInfo(itemLink)
+
+		-- Retrieve the itemID from the itemLink
+		local itemID = tonumber(string_match(itemLink, "item:(%d+)"))
+
+		---------------------------------------------------
+		-- ItemBind
+		---------------------------------------------------
+		if (itemRarity and (itemRarity > 1)) and ((bindType == 2) or (bindType == 3)) and (not IsItemBound(self)) then
+			local ItemBind = Cache_ItemBind[self] or Cache_GetItemBind(self)
+			local r, g, b = GetItemQualityColor(itemRarity)
+			ItemBind[self]:SetTextColor(r * 2/3, g * 2/3, b * 2/3)
+			ItemBind[self]:SetText((bindType == 3) and L["BoU"] or L["BoE"])
+		else 
+			if Cache_ItemBind[self] then 
+				Cache_ItemBind[self]:SetText("")
+			end	
+		end
+
+	else
+		if Cache_ItemBind[self] then 
+			Cache_ItemBind[self]:SetText("")
+		end	
+	end	
+end 
+
+Module.OnEnable = function(self)
+	hooksecurefunc(Bagnon.ItemSlot, "Update", Update)
+end 
