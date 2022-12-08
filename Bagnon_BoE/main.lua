3aa709e7cb6ef83be81e5286974c50fef475e775
Lars Norberg <github@larsnorbergofficial.com>
2022-12-08 08:48:13 +0100

    check for tooltipData line existence

--- Bagnon_BoE/main.lua
+++ Bagnon_BoE/main.lua
@@ -121,7 +121,7 @@ Module:AddUpdater(function(self)
 					end
 
 					for i = 2,6 do
-						local msg = tooltipData.lines[i].leftText
+						local msg = tooltipData.lines[i] and tooltipData.lines[i].leftText
 						if (not msg) then break end
 
 						if (string_find(msg, s_item_bound1) or string_find(msg, s_item_bound2) or string_find(msg, s_item_bound3)) then
